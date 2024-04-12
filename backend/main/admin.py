7ad8bec519123ba7bf041a4abbbf1250a6fda54c
DaniruBu <buda-0000@mail.ru>
2024-04-12 19:10:23 +0300

    fixtures

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from main.models import *
 
-# Register your models here.
+admin.site.register(Skill)
+
+admin.site.register(Hobby)
+admin.site.register(Club)
+admin.site.register(TypeImportance)
+admin.site.register(TypeEvent)
+admin.site.register(Events)
+admin.site.register(News)
+admin.site.register(User)
+admin.site.register(Place)
+admin.site.register(Task)
+admin.site.register(Schedule)
